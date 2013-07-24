b2c903b07644f50edb4073725650cff0bc991eb8
marat <marat@marat-Inspiron-N5110.(none)>
2013-07-24 10:45:18 +0400

    rails new empty project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsDemo::Application.config.secret_key_base = '389f4b3d35aad567c7d51f4877dd97eeb148452c65dc2896b90f9a3ac02108eee7237cada657010a2d7289aea9c5616ecd69b7dfb4a4b9b53013783bf795a338'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RailsDemo::Application.config.session_store :encrypted_cookie_store, key: '_rails_demo_session'
